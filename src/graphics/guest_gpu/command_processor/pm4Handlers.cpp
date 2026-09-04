71f2cd0843235758f016f18c2e2036796d6c3c3f
Ali Almohaya <AliAlmohaya@gmail.com>
2026-09-04 13:30:14 +0300

    perf(gpu): reduce synchronization overhead
    Avoid redundant predication waits, rate-limit timeline semaphore refreshes, and batch end-of-pipe fence submissions.

--- src/graphics/guest_gpu/command_processor/pm4Handlers.cpp
+++ src/graphics/guest_gpu/command_processor/pm4Handlers.cpp
@@ -2292,7 +2292,7 @@ KYTY_CP_OP_PARSER(CpOpReleaseMem) {
 			default: EXIT("unknown release_mem interrupt selector\n");
 		}
 		if (queued) {
-			cp.BufferFlush();
+			cp.RequestBufferFlush();
 		}
 	};
 
@@ -2331,7 +2331,7 @@ KYTY_CP_OP_PARSER(CpOpReleaseMem) {
 		cp.WriteAtEndOfPipe32(cache_policy, event_write_dest, eop_event_type, cache_action,
 		                      event_index, event_source, dst_gpu_addr, static_cast<uint32_t>(value),
 		                      interrupt_selector, interrupt_context_id);
-		cp.BufferFlush();
+		cp.RequestBufferFlush();
 
 		return 7;
 	}
@@ -2349,7 +2349,7 @@ KYTY_CP_OP_PARSER(CpOpReleaseMem) {
 		                      event_index, event_source, dst_gpu_addr, static_cast<uint32_t>(value),
 		                      interrupt_selector, interrupt_context_id);
 		if (interrupt_selector == 0x01) {
-			cp.BufferFlush();
+			cp.RequestBufferFlush();
 		}
 
 		return 7;
